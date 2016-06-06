12d9585ae0a1297e9487fb0c97e6e77da262f5f8
s9202259 <s9202259@yahoo.com.tw>
2016-06-06 15:57:26 +0800

    finish program
    complete the program
    add annotation

--- NumberPr.java
+++ NumberPr.java
@@ -1,6 +1,7 @@
 //U10416030 陳子勤
 
 import java.util.*;
+import java.math.*;
 
 public class NumberPr{
 	public static void main(String[] args){
@@ -8,16 +9,46 @@ public class NumberPr{
 		
 		String number1;
 		String number2;
-		int action;
+		int action;	//to decide what user want to do
 		
 		System.out.println("Enter number1: ");
 		number1 = input.next();
 		
 		System.out.println("Enter number2: ");
 		number2 = input.next();
+
+		Number number = new Number(number1,number2);
 		
 		System.out.println("Choose an action:(1 to plus, 2 to subtract, 3 to multiply, 4 to divide)");
 		action = input.nextInt();
 		
+		System.out.println("Result: " + number.calculate(action));
 	}
 }
+
+class Number {
+	BigDecimal number1;
+	BigDecimal number2;
+	
+	Number(String a, String b) {
+		number1 = new BigDecimal(a);
+		number2 = new BigDecimal(b);
+	}
+	
+	//calculate and return result
+	String calculate(int action){
+		String result = "";
+		if(action == 1)
+			result = String.valueOf(number1.add(number2));				
+		else if(action == 2)
+			result = String.valueOf(number1.subtract(number2));
+		else if(action == 3)
+			result = String.valueOf(number1.multiply(number2));
+		else if(action == 4)			
+			result = String.valueOf(number1.divide(number2, 5, BigDecimal.ROUND_HALF_UP));
+		else
+			result = "ERROR";
+		return result;
+	}
+}
+
