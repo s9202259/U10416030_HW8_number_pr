d4d704a0fd946e555ddd47577e70ea2531409622
s9202259 <s9202259@yahoo.com.tw>
2016-06-06 15:53:57 +0800

    create class
    create basic datafields

---
+++ NumberPr.java
@@ -0,0 +1,23 @@
+//U10416030 陳子勤
+
+import java.util.*;
+
+public class NumberPr{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		String number1;
+		String number2;
+		int action;
+		
+		System.out.println("Enter number1: ");
+		number1 = input.next();
+		
+		System.out.println("Enter number2: ");
+		number2 = input.next();
+		
+		System.out.println("Choose an action:(1 to plus, 2 to subtract, 3 to multiply, 4 to divide)");
+		action = input.nextInt();
+		
+	}
+}
